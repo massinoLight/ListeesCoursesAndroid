cc77470731815b17fb8b8dff048ccf40e5c07b37
massino <nacerddine.amassin@gmail.com>
2020-04-21 03:04:30 +0200

    commit authentification avec google marche bien authentification avec facebook marche bien aussi

--- app/src/main/java/com/massino/listedescourse/MainActivity.kt
+++ app/src/main/java/com/massino/listedescourse/MainActivity.kt
@@ -1,257 +1,150 @@
 package com.massino.listedescourse
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Toast
-import com.facebook.AccessToken
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
-import com.facebook.login.LoginResult
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.firebase.auth.FacebookAuthProvider
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_firebase_ui.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
-    private lateinit var googleSignInClient: GoogleSignInClient
-    private lateinit var callbackManager: CallbackManager
-        companion object {
-        private const val TAG = "GoogleActivity"
-        private const val RC_SIGN_IN = 9001
-        private  var choix=0
+    companion object {
+
+        private const val RC_SIGN_IN = 123
     }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
-        auth = FirebaseAuth.getInstance()
-
-        callbackManager = CallbackManager.Factory.create()
-
-        login_facebook.setReadPermissions("email")
-        login_facebook.setPermissions("email")
-        login_facebook.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
-            override fun onSuccess(loginResult: LoginResult) {
-                Log.d(TAG, "facebook:onSuccess:$loginResult")
-                handleFacebookAccessToken(loginResult.accessToken)
-            }
-
-            override fun onCancel() {
-                Log.d(TAG, "facebook:onCancel")
-                updateUIFb(null)
-
-            }
-
-            override fun onError(error: FacebookException) {
-                Log.d(TAG, "facebook:onError", error)
-                updateUIFb(null)
-
-            }
-        })
+        setContentView(R.layout.activity_firebase_ui)
+        signOutButton.visibility= View.GONE
+        status.setText("vous etes déconnécté")
+        detail.text = null
 
         signInButton.setOnClickListener {
-            choix=1
-            signIn()
+            createSignInIntent()
         }
+
         signOutButton.setOnClickListener{
-            choix=1
             signOut()
         }
 
-        login_facebook.setOnClickListener{
-            signOut()
-        }
-
-
-
-
-
     }
 
+    private fun createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.FacebookBuilder().build())
 
-    public override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        if (choix==0){
-            updateUIGoogle(currentUser)
-        }else{
-            updateUIFb(currentUser)
-
-        }
-
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+        // [END auth_fui_create_intent]
     }
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+    // [START auth_fui_result]
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(choix==1) {
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
 
-            // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-            if (requestCode == RC_SIGN_IN) {
-                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-                try {
-                    // Google Sign In was successful, authenticate with Firebase
-                    val account = task.getResult(ApiException::class.java)
-                    firebaseAuthWithGoogle(account!!)
-                } catch (e: ApiException) {
-                    // Google Sign In failed, update UI appropriately
-                    Log.w(TAG, "Google sign in failed", e)
-                    // [START_EXCLUDE]
-                    updateUIGoogle(null)
-                    // [END_EXCLUDE]
-                }
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                     if (user != null) {
+
+                    status.text = "Google User email: " + user.email!!
+                    detail.text = "Firebase User ID: " + user.uid
+
+                    signInButton.visibility = View.GONE
+                    signOutButton.visibility = View.VISIBLE
+                    }else {
+                         status.text = "probléme survenu"
+                         detail.text = "probléme suvenu"
+
+                         signInButton.visibility = View.VISIBLE
+                         signOutButton.visibility = View.GONE
+                     }
+
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
             }
-        }else {
-
-            callbackManager.onActivityResult(requestCode, resultCode, data)
         }
-
     }
-    private fun handleFacebookAccessToken(token: AccessToken) {
-        Log.d(TAG, "handleFacebookAccessToken:$token")
-
-
-        val credential = FacebookAuthProvider.getCredential(token.token)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithCredential:success")
-                    val user = auth.currentUser
-                    updateUIFb(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithCredential:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-                    updateUIFb(null)
-                }
-
-
-            }
-    }
-    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.id!!)
-
-
-        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithCredential:success")
-                    val user = auth.currentUser
-                    updateUIGoogle(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithCredential:failure", task.exception)
-                    //Snackbar.make(main_layout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
-                    updateUIGoogle(null)
-                }
-
-
-            }
-    }
-
-    private fun signIn() {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
-    }
+    // [END auth_fui_result]
 
     private fun signOut() {
-
-        auth.signOut()
-
-        // Google sign out
-        googleSignInClient.signOut().addOnCompleteListener(this) {
-
-            if (choix==0){
-                updateUIGoogle(null)
-            }else{
-            updateUIFb(null)
-
-        }
-        }
-    }
-
-    private fun revokeAccess() {
-        // Firebase sign out
-        auth.signOut()
-
-        // Google revoke access
-        googleSignInClient.revokeAccess().addOnCompleteListener(this) {
-
-            if (choix==0){
-                updateUIGoogle(null)
-            }else{
-            updateUIFb(null)
-
-        }
-        }
-    }
-
-    private fun updateUIGoogle(user: FirebaseUser?) {
-
-            if (user != null) {
-
-                status.text = "Google User email: " + user.email!!
-                detail.text = "Firebase User ID: " + user.uid
-
-                signInButton.visibility = View.GONE
-                signOutButton.visibility = View.VISIBLE
-            } else {
-                status.setText("signed_out")
-                detail.text = null
-
-                signInButton.visibility = View.VISIBLE
-                signOutButton.visibility = View.GONE
+        // [START auth_fui_signout]
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                // ...
             }
+        // [END auth_fui_signout]
+        status.setText("vous etes déconnécté")
+        detail.text = null
 
-        }
-
-
-    private fun updateUIFb(user: FirebaseUser?) {
-            if (user != null) {
-                status.text =  user.displayName
-                detail.text = user.uid
-
-                login_facebook.visibility = View.GONE
-                signOutButton.visibility = View.VISIBLE
-            } else {
-                status.setText("signed_out")
-                detail.text = null
-
-                login_facebook.visibility = View.VISIBLE
-                signOutButton.visibility = View.GONE
-
-
-
-        }
+        signInButton.visibility = View.VISIBLE
+        signOutButton.visibility = View.GONE
     }
 
+    private fun delete() {
+        // [START auth_fui_delete]
+        AuthUI.getInstance()
+            .delete(this)
+            .addOnCompleteListener {
+                // ...
+            }
+        // [END auth_fui_delete]
+    }
 
+    private fun themeAndLogo() {
+        val providers = emptyList<AuthUI.IdpConfig>()
 
+        // [START auth_fui_theme_logo]
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.mipmap.ic_launcher_round) // Set logo drawable
+                .setTheme(R.style.MySuperAppTheme) // Set theme
+                .build(),
+            RC_SIGN_IN)
+        // [END auth_fui_theme_logo]
+    }
 
-
+    private fun privacyAndTerms() {
+        val providers = emptyList<AuthUI.IdpConfig>()
+        // [START auth_fui_pp_tos]
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTosAndPrivacyPolicyUrls(
+                    "https://example.com/terms.html",
+                    "https://example.com/privacy.html")
+                .build(),
+            RC_SIGN_IN)
+        // [END auth_fui_pp_tos]
+    }
 
 
 }
+
